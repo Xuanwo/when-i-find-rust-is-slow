0d40a12fcfaab1e5240936cb880dbed6560dc25a
Xuanwo <github@xuanwo.io>
2023-11-29 13:02:46 +0800

    Add rust-std-fs-read-with-jemalloc
    Signed-off-by: Xuanwo <github@xuanwo.io>

---
+++ rust-std-fs-read-with-jemalloc/src/main.rs
@@ -0,0 +1,22 @@
+use std::io::Read;
+use std::fs::OpenOptions;
+
+#[global_allocator]
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+fn main() {
+    let mut bs = vec![0; 64 * 1024 * 1024];
+    let mut f = OpenOptions::new().read(true).open("/tmp/file").unwrap();
+    let mut ts = 0;
+    loop {
+        let buf = &mut bs[ts..];
+        let n = f.read(buf).unwrap();
+        let n = n as usize;
+        if n == 0 {
+            break
+        }
+        ts += n;
+    }
+
+    assert_eq!(ts, 64 * 1024 * 1024);
+}
